c9b2760f0bdd45dd6a3dc5e57d4e6b098cd80f1c
Unknown <sunmay24@gmail.com>
2018-09-20 15:48:38 +0530

    Added D3 visualization and search bar

--- app/app.py
+++ app/app.py
@@ -6,19 +6,10 @@ app = Flask(__name__)
 
 # routes
 
-@app.route('/hello')
-def hello():
-    return "Hello, World!"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        value_one = int(request.form.get('first'))
-        value_two = int(request.form.get('second'))
-        total = value_one + value_two
-        data = {"total": str(total)}
-        return jsonify(data)
     return render_template('index.html')
 
 # run the server
