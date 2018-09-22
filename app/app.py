20b335537e85dda35e9556e837353ccc376642ed
echodarkstar <nishantshankar226@gmail.com>
2018-09-23 00:23:15 +0530

    Add pagila db connection

--- app/app.py
+++ app/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import Actor, Film
 
+from config import *
 # initilize flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
+db = SQLAlchemy(app)
+db.create_all()
+db.session.commit()
 
 # routes
 
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    actors = db.session.query(Actor).all()
+    return render_template('index.html', actors=actors)
 
 # run the server
 if __name__ == '__main__':
