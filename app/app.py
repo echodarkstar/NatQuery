0ab2e2cbd4e87b25bd059dd8a609fde96020a408
echodarkstar <nishantshankar226@gmail.com>
2018-09-23 12:54:26 +0530

    Add sql text query support

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from models import Actor, Film
+# from models import Actor, Film
 
 from config import *
+
+from sqlalchemy import text
+
 # initilize flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
@@ -15,8 +18,10 @@ db.session.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    actors = db.session.query(Actor).all()
-    return render_template('index.html', actors=actors)
+    #actors = db.session.query(Actor).all()
+    sql = text("select * from Film where film_id = 20")
+    result = db.engine.execute(sql)
+    return render_template('index.html', result=result)
 
 # run the server
 if __name__ == '__main__':
