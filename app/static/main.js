c4724f14a317a1b240cf28915ef6f9cf025933a4
Sunmay Agharkar <sunmay24@gmail.com>
2018-09-27 13:35:36 +0530

    Added animations

--- app/static/main.js
+++ app/static/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   console.log("ready!");
 });
 
-var main_query,related_queries,output,interpreted_words;
+var main_query, related_queries, output, interpreted_words;
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -25,7 +25,7 @@ const dictate = () => {
 }
 
 function search() {
-  $.getJSON("static/example.json",function(data){
+  $.getJSON("static/example.json", function (data) {
     var main_query = data["main_query"]["ques"];
     var interpreted_words = data["main_query"]["interpreted_words"];
     var output = data["main_query"]["output"];
@@ -35,46 +35,52 @@ function search() {
     // document.getElementById("speech_box").value = main_query;
 
     //creating related queries div outside the main
-    document.getElementById("main").setAttribute("class","col-md-6");
+    document.getElementById("main").setAttribute("class", "col-md-6");
     var related_box = document.createElement("div");
-    related_box.setAttribute("class","col-md-4");
+    related_box.setAttribute("class", "col-md-4");
     document.getElementById("container").appendChild(related_box);
 
     //populating div with JSON
 
     var related_div = document.createElement("div");
-    related_div.setAttribute("class","panel panel-info");
+    related_div.setAttribute("class", "panel panel-info");
     // related_div.setAttribute("class","panel-info");
     var related_div_head = document.createElement("div");
-    related_div_head.setAttribute("class","panel-heading");
+    related_div_head.setAttribute("class", "panel-heading");
     related_div_head.innerHTML = "Related Queries";
     var related_div_body = document.createElement("div");
-    related_div_body.setAttribute("class","panel-body");
-    
+    related_div_body.setAttribute("class", "panel-body");
+
     //iterating over related queries JSON
-    var arr = ["0","1","2"]
-    arr.forEach(function(value){
+    var arr = ["0", "1", "2"]
+    arr.forEach(function (value) {
       var related_div_body_parts = document.createElement("div");
-      related_div_body_parts.setAttribute("class"," panel panel-info panel-body");
-      related_div_body_parts.setAttribute("id","ques"+value);
-      related_div_body_parts.innerHTML = related_queries[value];
+      related_div_body_parts.setAttribute("class", " panel panel-info panel-body");
+      related_div_body_parts.setAttribute("id", "ques" + value);
+      var inner_text = document.createElement("div");
+      inner_text.setAttribute("id", "text" + value);
+      inner_text.setAttribute("data-y", parseInt(value));
+      inner_text.innerText = related_queries[value];
+      related_div_body_parts.appendChild(inner_text);
+      // related_div_body_parts.innerHTML = related_queries[value];
+      related_div_body_parts.setAttribute("onclick", "animated(this.id)");
       related_div_body.appendChild(related_div_body_parts);
       // document.getElementById("related_box").appendChild(related_div);
-    }); 
+    });
     related_div.appendChild(related_div_head);
     related_div.appendChild(related_div_body);
     related_box.appendChild(related_div);
-    
+
     //Interpreted words div
 
     var interpreted_div = document.createElement("div");
-    interpreted_div.setAttribute("class","panel panel-info");
+    interpreted_div.setAttribute("class", "panel panel-info");
     // interpreted_div.setAttribute("class","panel-info");
     var interpreted_div_head = document.createElement("div");
-    interpreted_div_head.setAttribute("class","panel-heading");
+    interpreted_div_head.setAttribute("class", "panel-heading");
     interpreted_div_head.innerHTML = "Interpreted words";
     var interpreted_div_body = document.createElement("div");
-    interpreted_div_body.setAttribute("class","panel-body");
+    interpreted_div_body.setAttribute("class", "panel-body");
     interpreted_div_body.innerHTML = interpreted_words;
     interpreted_div.appendChild(interpreted_div_head);
     interpreted_div.appendChild(interpreted_div_body);
@@ -83,13 +89,13 @@ function search() {
     //SQL query
 
     var query_div = document.createElement("div");
-    query_div.setAttribute("class","panel panel-info");
+    query_div.setAttribute("class", "panel panel-info");
     // interpreted_div.setAttribute("class","panel-info");
     var query_div_head = document.createElement("div");
-    query_div_head.setAttribute("class","panel-heading");
+    query_div_head.setAttribute("class", "panel-heading");
     query_div_head.innerHTML = "SQL query";
     var query_div_body = document.createElement("div");
-    query_div_body.setAttribute("class","panel-body");
+    query_div_body.setAttribute("class", "panel-body");
     query_div_body.innerHTML = main_query;
     query_div.appendChild(query_div_head);
     query_div.appendChild(query_div_body);
@@ -98,13 +104,13 @@ function search() {
     //Output div
 
     var output_div = document.createElement("div");
-    output_div.setAttribute("class","panel panel-info");
+    output_div.setAttribute("class", "panel panel-info");
     // output_div.setAttribute("class","panel-info");
     var output_div_head = document.createElement("div");
-    output_div_head.setAttribute("class","panel-heading");
+    output_div_head.setAttribute("class", "panel-heading");
     output_div_head.innerHTML = "Output";
     var output_div_body = document.createElement("div");
-    output_div_body.setAttribute("class","panel-body");
+    output_div_body.setAttribute("class", "panel-body");
     output_div_body.innerHTML = output;
     output_div.appendChild(output_div_head);
     output_div.appendChild(output_div_body);
@@ -112,5 +118,36 @@ function search() {
   })
 }
 
+console.log(document.getElementById("speech_box").clientHeight);
+console.log(document.getElementById("voice").clientHeight);
+
 //Animation functions
 
+function animated(click_id) {
+  // console.log(click_id[4]);
+  var animate = anime({
+    targets: '#text' + click_id[4],
+    translateX: [{
+      value: '-41em',
+      duration: 1200
+    }],
+    translateY: function (target) {
+      return -10 - 51 * target.getAttribute("data-y");
+    },
+    easing: 'easeOutQuart',
+    complete: function (anim) {
+      console.log("completed", anim.completed);
+      animated2(click_id);
+
+    }
+  });
+
+}
+
+function animated2(click_id) {
+  // console.log("#text"+click_id[4]);
+  // console.log(document.getElementById("#text"+click_id[4]));
+  document.getElementById("speech_box").value = document.getElementById("text" + click_id[4]).innerHTML;
+  document.getElementById("text" + click_id[4]).style.display = "none";
+  // console.log("done");
+}
